60ba44d2347b3689180adcbdc9d73f32b20ef7cf
SOPI Research Group <sopi.research@gmail.com>
2014-06-02 15:04:17 +0300

    License update

---
+++ SOPISupportingFiles/main.m
@@ -0,0 +1,31 @@
+/*
+ *
+ * Created by: Koray Tahiroğlu, Miguel Valero Espada, Nuno Correia, James Nesfield;
+ * Academy of Finland (project 137646) The Notion of Participative and Enacting Sonic Interaction - PESI
+ * SOPI research group, Aalto University, School of Arts, Design and Architecture
+ *
+ * Copyright (c) 2013 Aalto University. All rights reserved. <koray.tahiroglu@aalto.fi>
+ *
+ * BSD Simplified License.
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ *
+ * See https://github.com/SopiMlab/ for documentation
+ *
+ */
+
+//
+//  main.m
+//  SOPImobile
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SOPIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SOPIAppDelegate class]));
+    }
+}
